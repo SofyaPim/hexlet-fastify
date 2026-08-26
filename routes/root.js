3bd813853b9964c619c598b1fed7a4453a1b5194
Sofya <sofanpim@mail.ru>
2026-08-26 15:26:48 +0300

    dinamyc routes

--- routes/root.js
+++ routes/root.js
@@ -1,10 +1,42 @@
 // const data = { phones: ['+123'], domains: ['test.com'] };
+const users = {
+  user: [
+    {
+      id: 1,
+      post: [
+        {
+          id:1,
+        text: 'some'
+        }
+        
+      ]
+    },
+  ],
+};
 export default async function (fastify, opts) {
   fastify.get('/hello', async function (request, reply) {  
   const name = request.query.name || 'World'
 
   reply.send(`Hello, ${name}!`)
 })
+// users/{id}/post/{postId}
+  fastify.get("/users/:id/post/:postId", (req, res) => {
+    const { id, postId}  = req.params;
+    const user = users.user.find((u) => u.id === Number(id));
+    if (!user) {
+      return res.code(404).send({ message: "User not found" });
+    }
+
+    const post = user.post.find((p) => p.id === Number(postId));
+    if (!post) {
+      return res.code(404).send({ message: "Post not found" });
+    }
+
+  
+    res.send(`User ID: ${user.id}; Post Text: ${post.text}`);
+   
+  });
+
   // fastify.get("/", async function (request, reply) {
   //   reply.send("Welcome to Hexlet!");
   // });
